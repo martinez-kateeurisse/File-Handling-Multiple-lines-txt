127c47441c19bbdecb18d044fbd34fc0d903e69d
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-23 17:26:01 +0800

    Pseudocode

---
+++ Multiple_line_text_file.py
@@ -0,0 +1,13 @@
+#Kate Eurisse L. Martinez_BSCPE1-5_Multiple line text contents file
+
+#A Python program that contains a method that writes multiple line of text contents into a text file mylife.txt
+
+#Create a text file named mylife.txt
+
+#Open the text file   
+    #Initialize the loop control variable
+    #Loop condition
+        #Ask the user to enter a line
+        #Append the line into the text file mylife.txt
+        #Ask the user if he/she wants to input another line
+    #Loop will end when condition is not met
