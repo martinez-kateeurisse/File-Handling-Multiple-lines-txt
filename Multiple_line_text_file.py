27c8d31224b0389b097deadaa99eb5e1e6183116
martinez-kateeurisse <martinezkateeurisse@gmail.com>
2023-04-25 02:01:01 +0800

    Greetings for user-added

--- Multiple_line_text_file.py
+++ Multiple_line_text_file.py
@@ -10,6 +10,11 @@ import prog_design
 prog_design.multiple_line_header()
 
 #Ask the user's name and printing a greeting
+print("//" * 20)
+name = input(f"{Fore.RED} Enter your name: "+ Fore.RESET)
+print("//"*20, "\n\n") 
+print(Back.LIGHTBLACK_EX, Fore.LIGHTCYAN_EX, ("Hello " + name).center(84, "*") + Back.RESET, "\n")
+
 #Display the program's instructions
 
 #Create a text file named mylife.txt
